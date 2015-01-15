16dc5eeb4a5dc29e2fd6f86de5d3806772781860
liamscofield <liamscofield@gmail.com>
2015-01-15 10:55:09 +0800

    WKWebview
    Demo for WKWebView on iOS8

--- WKWebViewDemo/ViewController.swift
+++ WKWebViewDemo/ViewController.swift
@@ -7,18 +7,74 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController,WKScriptMessageHandler {
+
+    var webView: WKWebView?
+    
+    override func loadView() {
+        super.loadView()
+        
+      
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        var contentController = WKUserContentController()
+        
+        var userScript = WKUserScript(
+            source: "redHeader()", injectionTime: WKUserScriptInjectionTime.AtDocumentEnd, forMainFrameOnly: true)
+        
+        contentController.addUserScript(userScript)
+        
+        
+        var config = WKWebViewConfiguration()
+        
+        config.userContentController = contentController
+        
+        self.webView = WKWebView(frame: self.view.bounds, configuration: config)
+        self.view.addSubview(webView!)
+        
+        
+
+        
+        
+        var sss = NSBundle.mainBundle().pathForResource("index", ofType: "html")
+        println(sss!)
+        var url = NSURL(fileURLWithPath: sss!)
+        
+        var req = NSURLRequest(URL: url!)
+        
+        self.webView!.loadRequest(req)
+        
+//        var uiweb = UIWebView(frame: self.view.bounds)
+//        
+//        self.view.addSubview(uiweb)
+//        
+//        uiweb.loadRequest(req)
+        
+        
+   
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func userContentController(userContentController: WKUserContentController, didReceiveScriptMessage message: WKScriptMessage){
+        if(message.name == "callbackHandler") {
+            println("JavaScript is sending a message \(message.body)")
+        }
+        
+
+    }
+
+
 
 
 }
